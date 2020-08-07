3bb510c24d15335d5097874d7013da50ed934fe5
Aliaksandr Pletsiazhou <alexandr.pletezhov@gmail.com>
2020-08-07 15:19:43 +0300

    feat: Custom Hooks

--- src/App.js
+++ src/App.js
@@ -3,37 +3,29 @@ import React, { useState, useEffect } from 'react';
 import Search from './components/search';
 import List from './components/list';
 
-const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+import stories from './data/stories';
 
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem('search') || 'React'
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
   );
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
 
   const searchedStories = stories.filter((story) => (
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
