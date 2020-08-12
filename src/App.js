8f085163ad6238c9bab47b8a9734988ab0c1618b
Aliaksandr Pletsiazhou <alexandr.pletezhov@gmail.com>
2020-08-12 14:55:29 +0300

    feat: Data Re-fetching

--- src/App.js
+++ src/App.js
@@ -22,9 +22,13 @@ const App = () => {
   );
 
   useEffect(() => {
+    if (!searchTerm) {
+      return;
+    }
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => (response.json()))
       .then((result) => {
         dispatchStories({
@@ -35,7 +39,7 @@ const App = () => {
       .catch(() => {
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
       });
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -47,10 +51,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) => (
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  ));
 
   return (
     <div>
@@ -73,7 +73,7 @@ const App = () => {
         ? <p>Loading ...</p>
         : (
           <List
-            list={searchedStories}
+            list={stories.data}
             onRemoveItem={handleRemoveStory}
           />
         )
